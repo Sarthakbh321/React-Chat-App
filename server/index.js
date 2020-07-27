ba3d1fe5e2040e48946c587a95c6d6e60c30ee73
Sarthak <sarthakbh321@gmail.com>
2020-07-27 22:49:36 +0530

    basic socket io server code

--- server/index.js
+++ server/index.js
@@ -4,10 +4,23 @@ const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 
 const io = socketio(server);
+
+io.on("connection", (socket) => {
+	console.log("New connection!");
+
+	socket.on("disconnect", () => {
+		console.log("User left :(");
+	})
+});
+
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server start on port ${PORT}`));
 
