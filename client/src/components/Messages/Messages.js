c8f8d1375b9ef2b3ea97fe2478268a623706df85
Sarthak <sarthakbh321@gmail.com>
2020-07-29 22:50:01 +0530

    scroll to bottom

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 
 function Messages(props) {
+	const divref = useRef(null);
+
+	useEffect(() => {
+		divref.current.scrollIntoView({behavior: "smooth"});
+	}, [props.messages])
 
 	return (
 		<ScrollToBottom>
 			{props.messages.map((message) => (
 				<p>{message.text}</p>
 			))}
+			<div ref={divref} />
 		</ScrollToBottom>
 	)
 
